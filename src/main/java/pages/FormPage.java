9084004ebf2de4472bcd49e5352dc9cfe7baca60
Dima Akulich <dima.akulicz@gmail.com>
2023-12-24 09:59:55 +0700

    LoginPage -> FormPage, removed unnecessary xpath from FormPage.

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -2,78 +2,56 @@ package pages;
 
 import BaseAbstractElements.BaseAbstractPage;
 import Core.TAEDriver;
-import Entities.WebDriverType;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LoginPage extends BaseAbstractPage {
+public class FormPage extends BaseAbstractPage {
     @FindBy(xpath = "//input[@id='name']")
     public WebElement nameInput;
-    public String nameInputX = "//input[@id='name']";
     @FindBy(xpath = "//input[@id='email']")
     public WebElement emailInput;
-    public String emailInputX = "//input[@id='email']";
     @FindBy(xpath = "//input[@id='phone']")
     public WebElement phoneInput;
-    public String phoneInputX = "//input[@id='phone']";
     @FindBy(xpath = "//input[@id='subject']")
     public WebElement subjectInput;
-    public String subjectInputX = "//input[@id='subject']";
     @FindBy(xpath = "//textarea[@id='description']")
     public WebElement messageTextarea;
-    public String messageTextareaX = "//textarea[@id='description']";
     @FindBy(xpath = "//button[@id='submitContact']")
     public WebElement submitButton;
-    public String submitButtonX = "//button[@id='submitContact']";
     @FindBy(xpath = "//div[contains(@class, 'contact')]//h2")
     public WebElement confirmationText;
-    public String confirmationTextX = "//div[contains(@class, 'contact')]//h2";
 
-    public LoginPage(TAEDriver driver) {
+    public FormPage(TAEDriver driver) {
         super(driver);
         PageFactory.initElements(driver.getDriver(), this);
     }
 
-    public void goToPage() {
-        driver.getDriver().get("https://automationintesting.online");
-    }
-
     public void setNameContent(String value) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(nameInputX)));
         nameInput.sendKeys(value);
     }
 
     public void setEmailContent(String value) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(emailInputX)));
         emailInput.sendKeys(value);
     }
 
     public void setPhoneContent(String value) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(phoneInputX)));
         phoneInput.sendKeys(value);
     }
 
     public void setSubjectContent(String value) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(subjectInputX)));
         subjectInput.sendKeys(value);
     }
 
     public void setMessageContent(String value) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(messageTextareaX)));
         messageTextarea.sendKeys(value);
     }
 
     public void clickButton() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(submitButtonX)));
         submitButton.click();
     }
 
     public String getConfirmationText() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(confirmationTextX)));
         return confirmationText.getText();
     }
 
--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -3,18 +3,19 @@ package tests;
 import BaseTest.BaseAbstractTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.LoginPage;
+import pages.FormPage;
 
 public class Test1 extends BaseAbstractTest {
+    public final String BASE_URL = "https://automationintesting.online";
     public Test1() {
         super("chrome", false);
     }
 
     @Test
-    public void submitFormWithAllParameters() throws InterruptedException {
-        LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.goToPage();
-
+    public void submitFormWithAllParameters() {
+        FormPage loginPage = new FormPage(getDriver());
+        getDriver().getDriver().get(BASE_URL);
+        String confirmationText = "Thanks for getting in touch %s!";
         String name = "Dima";
         String email = "a@a.com";
         String phone = "12345678333390";
@@ -24,12 +25,9 @@ public class Test1 extends BaseAbstractTest {
         loginPage.setEmailContent(email);
         loginPage.setPhoneContent(phone);
         loginPage.setSubjectContent(subject);
-
-        Thread.sleep(1000);
         loginPage.setMessageContent(message);
         loginPage.clickButton();
-        Thread.sleep(3000);
-        Assert.assertEquals(loginPage.getConfirmationText(), String.format("Thanks for getting in touch %s!", name));
+        Assert.assertEquals(loginPage.getConfirmationText(), String.format(confirmationText, name));
     }
 
 }
