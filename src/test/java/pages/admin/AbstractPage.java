78e0ee035740760e06b58eb4ed58c4c8a3043913
mikhail <micktop@icloud.com>
2024-02-11 12:20:00 +0700

    sx

--- src/test/java/pages/admin/AbstractPage.java
+++
@@ -1,9 +0,0 @@
-package pages.admin;
-
-import BaseAbstractElements.BaseAbstractPage;
-import Core.TAEDriver;
-
-public class AbstractPage extends BaseAbstractPage {
-    public final String BASE_URL = "https://sklep165422.shoparena.pl/admin";
-
-}
